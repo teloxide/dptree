f05279811934e480a926165c31e05ce49fa7b399
p0lunin <dmytro.polunin@gmail.com>
2021-12-11 12:09:54 +0200

    remove middleware

--- src/handler.rs
+++ src/handler.rs
@@ -3,10 +3,8 @@ mod core;
 mod endpoint;
 mod filter;
 mod inserter;
-mod middleware;
 
 pub use self::core::*;
 pub use endpoint::*;
 pub use filter::*;
 pub use inserter::*;
-pub use middleware::*;
