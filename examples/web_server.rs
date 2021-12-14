c0b1beeaf521dd8f7a653481afc642edd93c0831
Hirrolot <hirrolot@gmail.com>
2021-12-14 07:32:47 +0600

    Refactor `examples/web_server.rs`

--- examples/web_server.rs
+++ examples/web_server.rs
@@ -1,37 +1,40 @@
 use dptree::{deps, di::DependencyMap, prelude::*};
-use regex::Regex;
 
 type Request = Arc<&'static str>;
+type Response = String;
+type WebHandler = Endpoint<'static, DependencyMap, Response>;
 
 #[tokio::main]
 async fn main() {
-    let smile_handler = dptree::filter(|req: Request| async move { req.starts_with("/smile") })
-        .endpoint(|| async { "🙃".to_owned() });
+    let web_server =
+        dptree::entry().branch(smiles_handler()).branch(sqrt_handler()).branch(not_found_handler());
 
-    let add_handler = dptree::filter_map(|req: Request| async move {
-        let re = Regex::new(r"/add (?P<a>\d) (?P<b>\d)").unwrap();
-        match re.captures(&req) {
-            Some(caps) => Some((caps["a"].parse::<i32>().ok()?, caps["b"].parse::<i32>().ok()?)),
-            _ => None,
+    let smile = web_server.dispatch(deps! { "/smile" }).await;
+    let sqrt_16 = web_server.dispatch(deps! { "/sqrt 16" }).await;
+    let not_found = web_server.dispatch(deps! { "/lol" }).await;
+
+    assert_eq!(smile, ControlFlow::Break("🙃".to_owned()));
+    assert_eq!(sqrt_16, ControlFlow::Break("4".to_owned()));
+    assert_eq!(not_found, ControlFlow::Break("404 Not Found".to_owned()));
+}
+
+fn smiles_handler() -> WebHandler {
+    dptree::filter(|req: Request| async move { req.starts_with("/smile") })
+        .endpoint(|| async { "🙃".to_owned() })
+}
+
+fn sqrt_handler() -> WebHandler {
+    dptree::filter_map(|req: Request| async move {
+        if req.starts_with("/sqrt") {
+            let (_, n) = req.split_once(" ")?;
+            n.parse::<f64>().ok()
+        } else {
+            None
         }
     })
-    .endpoint(|numbers: Arc<(i32, i32)>| async move {
-        let (a, b) = *numbers;
-        format!("{} + {} is {}", a, b, a + b)
-    });
+    .endpoint(|n: Arc<f64>| async move { format!("{}", n.sqrt()) })
+}
 
-    let not_found_handler = dptree::endpoint(|| async { "404 Not Found".to_owned() });
-
-    let web_server =
-        dptree::entry().branch(smile_handler).branch(add_handler).branch(not_found_handler);
-
-    assert_eq!(web_server.dispatch(deps! { "/smile" }).await, ControlFlow::Break("🙃".to_owned()));
-    assert_eq!(
-        web_server.dispatch(deps! { "/add 3 5" }).await,
-        ControlFlow::Break("3 + 5 is 8".to_owned())
-    );
-    assert_eq!(
-        web_server.dispatch(deps! { "/lol" }).await,
-        ControlFlow::Break("404 Not Found".to_owned())
-    );
+fn not_found_handler() -> WebHandler {
+    dptree::endpoint(|| async { "404 Not Found".to_owned() })
 }
